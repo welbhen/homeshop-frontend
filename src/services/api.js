3ca02e2b1027fab4d7a05a479f5b0e748b187907
Welberth <welberth_henrique@hotmail.com>
2022-04-14 15:10:57 -0300

    Updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    //baseURL: 'http://localhost:8000'
+    baseURL: 'https://homeshop-backend.herokuapp.com/'
 });
 
 export default api;
